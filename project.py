b79ad54afc5a38ac209d2228e0c49275c2ba2ae6
Pratim Mukherjee <86236049+Pratim-Mukherjee@users.noreply.github.com>
2021-06-21 18:37:15 +0530

    1st assignment.

---
+++ project.py
@@ -0,0 +1,11 @@
+
+num1= int(input('enter the 1st no:'))
+num2= int(input('enter the 2nd number'))
+multi = num1 * num2
+add = num1 + num2
+sub = num1 - num2
+
+if multi >= 1200:
+    print('The sum of the numbers are: ',add  )
+else:
+    print('The difference of the numbers are:',sub)
