5861edcba8c2d27309db8dc85d857d042bf3bfb4
Pratim Mukherjee <86236049+Pratim-Mukherjee@users.noreply.github.com>
2021-06-21 20:51:12 +0530

    project3

---
+++ project3.py
@@ -0,0 +1,13 @@
+
+string = "I want to hear from you. What do you think of the basic python exercise:"
+substring = "you"
+
+count = string.count(substring)
+
+# print count
+print("The you is:", count)
+
+
+
+
+
